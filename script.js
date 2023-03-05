467d593803c2b6c32f236371f013ec1c87f14c39
João Vieira <38960861+joaovieira85@users.noreply.github.com>
2023-03-05 20:33:20 +0000

    Getting the latitude and longitude (#1)

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      let { latitude } = pos.coords;
+      let { longitude } = pos.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      console.log(latitude, longitude);
+    },
+    function () {
+      console.log(alert('Don´t have acess to your localization'));
+    }
+  );
+}
